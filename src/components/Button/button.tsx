e4ca9d96354ec63c44cc80c22534e074f2e8febc
henrique26 <fabricioohh@gmail.com>
2024-06-02 17:55:45 -0300

    Design system with Restyle - component Button

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -24,7 +24,7 @@ export function Button({
   const buttonPreset = buttonPesets[preset][disabled ? 'disabled' : 'default']
   return (
     <TouchableOpacityBox
-    disabled={disabled || loading}
+      disabled={disabled || loading}
       paddingHorizontal='s20'
       height={50}
       alignItems="center"
