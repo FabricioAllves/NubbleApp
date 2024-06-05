cabe3a0881e54258029a789bf56f1c1230d653cd
henrique26 <fabricioohh@gmail.com>
2024-06-05 19:16:13 -0300

    Component Icon

--- App.tsx
+++ App.tsx
@@ -1,31 +1,43 @@
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-} from 'react-native';
-import { Text } from './src/components/Text/Text';
-import { Button } from './src/components/Button/button';
-import { ThemeProvider } from '@shopify/restyle';
+import {SafeAreaView, View} from 'react-native';
+
+import {Box} from './src/components/Box/Box';
+import {Icon} from './src/components/Icon/Icon';
+
+import {Text} from './src/components/Text/Text';
 import { theme } from './src/theme/theme.';
-import { Box } from './src/components/Box/Box';
 
-function App(): React.JSX.Element {
 
+function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View style={{ paddingHorizontal: 24 }}>
-          <Text preset='headingLarge'>Coffstack</Text>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" italic>
+            Coffstack
+          </Text>
 
-          <Button title='Entrar' marginBottom='s12'/>
-          <Button preset='outline' title='outline' marginBottom='s12'/>
-          <Button title='Loading' loading={true} marginBottom='s12'/>
-
+          <Box flexDirection="row">
+            <Icon name="chevronRight" size={50} />
+            <Icon name="heartFill" color="buttonPrimary" />
+            <Icon name="profile" size={50} />
+            <Icon name="profileFill" size={50} />
+            <Icon name="heart" size={50} />
+            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          </Box>
+          <Box flexDirection="row">
+            <Icon name="newPost" size={50} />
+            <Icon name="camera" size={50} />
+            <Icon name="chat" size={50} />
+            <Icon name="chatOn" color="error" size={50} />
+            <Icon name="flashOff" size={50} />
+            <Icon name="flashOn" size={50} />
+          </Box>
         </View>
       </SafeAreaView>
     </ThemeProvider>
   );
 }
-
 
 export default App;
