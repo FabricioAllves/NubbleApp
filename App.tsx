1a5d5dbc8fb8a8094b934ebf2921fa73104088e3
henrique26 <fabricioohh@gmail.com>
2024-06-06 21:49:57 -0300

    feat: create textInput component

--- App.tsx
+++ App.tsx
@@ -1,39 +1,49 @@
-import {ThemeProvider} from '@shopify/restyle';
+import { ThemeProvider } from '@shopify/restyle';
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 
-import {Box} from './src/components/Box/Box';
-import {Icon} from './src/components/Icon/Icon';
+import { Box } from './src/components/Box/Box';
+import { Icon } from './src/components/Icon/Icon';
 
-import {Text} from './src/components/Text/Text';
+import { Text } from './src/components/Text/Text';
 import { theme } from './src/theme/theme.';
+import { Button } from './src/components/Button/button';
+import { TextInput } from './src/components/TextInput/TextInput';
 
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            Coffstack
+        <View style={{ paddingHorizontal: 24 }}>
+          <Text mb='s8' preset="headingLarge">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb='s40'>
+            Digite seu e-mail e senha para entrar
           </Text>
 
-          <Box flexDirection="row">
-            <Icon name="chevronRight" size={50} />
-            <Icon name="heartFill" color="buttonPrimary" />
-            <Icon name="profile" size={50} />
-            <Icon name="profileFill" size={50} />
-            <Icon name="heart" size={50} />
-            <Icon name="bellOn" color="carrotSecondary" size={50} />
-          </Box>
-          <Box flexDirection="row">
-            <Icon name="newPost" size={50} />
-            <Icon name="camera" size={50} />
-            <Icon name="chat" size={50} />
-            <Icon name="chatOn" color="error" size={50} />
-            <Icon name="flashOff" size={50} />
-            <Icon name="flashOn" size={50} />
-          </Box>
+          
+            <TextInput
+            label='E-mail'
+            placeholder='Digite seu e-mail'
+            boxProps={{mb: 's20'}}
+            />
+          
+            <TextInput
+            errorMessage='Menssagem de erro'
+            label='Senha'
+            placeholder='Digite sua senha'
+            rightComponent={<Icon color='gray2' name='eyeOn'/>}
+            boxProps={{mb: 's10'}}
+            />
+
+          <Text color='primary' preset="paragraphSmall" bold mt='s10'>
+            Esqueci minha senha
+          </Text>
+
+          <Button title='Entrar' mt='s48'/>
+          <Button preset='outline' title='Criar uma conta' mt='s12'/>
         </View>
       </SafeAreaView>
     </ThemeProvider>
