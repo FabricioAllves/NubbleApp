9e6de1a25c3e43f502e18dfcf5a6ff499740d03b
FabricioAllves <fabricioohh@gmail.com>
2024-08-24 15:18:12 -0300

    update

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,9 +1,19 @@
-import {postApi} from './postApi';
+import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  const postList = await postApi.getList();
-  return postList;
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer NQ.MrTYdemsnesmIDgpEmArfAU6PyqPNuO-Ih6jAC-uNIjw8bDlCptQhtQGsWac',
+    },
+  });
+
+  let data = await response.json();
+  console.log('FETCH DATA:', data);
+
+  return postListMock;
 }
 
 export const postService = {
