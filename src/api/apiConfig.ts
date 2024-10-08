1f57ffdbf10a21d07458d6e275a6fbe34698e180
FabricioAllves <fabriciooh@gmail.com>
2024-10-08 00:02:48 -0300

    Dominio, API e Service

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer Mw.rrU9XY7phFsAxGgwPG4h0NVLy3GZ7ZFZtZZLGlIqhkC_sKCgcP-Lj1lMWFMX',
+      'Bearer MQ.CbsjsWXZnqtEIINFuHTM8RFo8nv89GPbndVi9JJ6zeKyL256CkKP1_rlOR_6',
   },
 });
