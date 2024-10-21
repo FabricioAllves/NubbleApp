2841ce88a726fdeb0dff209398aa9c69e0100ae6
FabricioAllves <fabriciooh@gmail.com>
2024-10-20 22:44:30 -0300

    refactor: ajustado imports

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {usePostCommentList} from 'src/domain/PostComment/useCases/usePostCommentList';
+import {usePostCommentList} from '@domain';
 
 import {Box, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
@@ -9,7 +9,7 @@ export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
   const postId = route.params.postId;
-  const {list} = usePostCommentList(postId);
+  usePostCommentList(postId);
 
   return (
     <Screen title="Comentários" canGoBack>
